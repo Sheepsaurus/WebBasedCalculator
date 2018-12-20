a6716ba4cadb3499431271e0496efc467cd0ed59
Sheep <Sheepsaurus@gmail.com>
2018-12-20 04:38:08 +0100

    Second Commit - Massive Progress

---
+++ Behind/WebBasedCalculatorBehind/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+
+namespace WebBasedCalculatorBehind
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Evaluate(args[0]));
+
+			Console.ReadKey();
+		}
+
+		public static double Evaluate(string expression)
+		{
+			DataTable table = new DataTable();
+			table.Columns.Add("expression", typeof(string), expression);
+			DataRow row = table.NewRow();
+			table.Rows.Add(row);
+			return double.Parse((string)row["expression"]);
+		}
+	}
+}
