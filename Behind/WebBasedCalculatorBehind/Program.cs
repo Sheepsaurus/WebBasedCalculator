37022a767b38024a3305d4a2d5d5b5942770cf6a
Alexander Kristensen <sheepsaurus@gmail.com>
2018-12-20 06:05:11 +0100

    LayoutWPF + Start of MVVM + Added Draw.IO

--- Behind/WebBasedCalculatorBehind/Program.cs
+++ Behind/WebBasedCalculatorBehind/Program.cs
@@ -5,19 +5,14 @@ namespace WebBasedCalculatorBehind
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine(Evaluate(args[0]));
+        static void Main(string[] args) => Console.WriteLine(Evaluate(args[0]));
 
-			Console.ReadKey();
-		}
-
-		public static double Evaluate(string expression)
-		{
-			DataTable table = new DataTable();
-			table.Columns.Add("expression", typeof(string), expression);
-			DataRow row = table.NewRow();
-			table.Rows.Add(row);
+        public static double Evaluate(string expression)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expression);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
 			return double.Parse((string)row["expression"]);
 		}
 	}
---
+++ WPFVariation/WPFVariation/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFVariation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string labelText;
+
+        public string LabelText 
+        { 
+            get { return labelText; } 
+            set 
+            {
+                labelText = value;
+                OnPropertyChanged();
+            }             
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            LabelText = "255";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button b)
+            {
+                MessageBox.Show(b.Content.ToString());
+                LabelText += b.Content.ToString();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        protected void OnPropertyChanged(
+        [System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
